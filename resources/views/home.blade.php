c8c1654daf58f8cdb8038beabbd543fa8a51a241
luigitarallo <tarallo.gigi@gmail.com>
2023-10-12 19:17:12 +0200

    feat: add foreach to print card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,3 +2,21 @@
 
 @section('page-title','Homepage')
 
+@section('main-content')
+<section class="container">
+    <div class="row row-cols-6 g-3">
+        @foreach ($comics as $comic)
+        <div class="col">
+            <div class="comic-img">
+                <img src="{{$comic['thumb']}}" alt="" class="img-fluid">
+            </div>
+            <p>{{$comic['title']}}</p>
+        </div>
+            
+        @endforeach
+
+    </div>
+</section>
+
+@endsection
+
